402bc25409bc96cc8d8f9b642fa25a91e4934295
Libor-1 <60381836+Libor-1@users.noreply.github.com>
2020-06-09 12:24:03 +0200

    Add files via upload

---
+++ wp1-10b.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+<?php
+for ($i = 1; $i <= 3; $i+= 1)  {  ?>
+
+    <div style="display: block; width:10px; height: 20px; background-color: #454d59;"></div>
+    <div style="display: block; width:20px; height: 20px; background-color: #454d59;"></div>
+    <div style="display: block; width:30px; height: 20px; background-color: #454d59;"></div>
+    <div style="display: block; width:40px; height: 20px; background-color: #454d59;"></div>
+    <div style="display: block; width:50px; height: 20px; background-color: #454d59;"></div>
+    <div style="display: block; width:60px; height: 20px; background-color: #454d59;"></div>
+    <div style="display: block; width:70px; height: 20px; background-color: #454d59;"></div>
+
+    <?php
+    }
+    ?> 
+
+</body>
+</html>
